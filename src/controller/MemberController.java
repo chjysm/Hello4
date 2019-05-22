714a4ce39cdad35f3ea422a16c8abad4ba0a3060
Jack <gml9407@naver.com>
2019-05-22 17:23:37 +0900

    T.T

--- src/controller/MemberController.java
+++ src/controller/MemberController.java
@@ -17,30 +17,7 @@ public class MemberController extends HttpServlet {
        
   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if(command.equals("logOut.member")) { // 로그아웃
-			request.getSession().invalidate();
-			request.getRequestDispatcher("logIndex.jsp").forward(request, response);
-		}if(command.equals("leave.member")) { //회원탈퇴 
-			String id = (String)request.getSession().getAttribute("loginID");
-			String pw = request.getParameter("password");
-			
-			int result = dao.login(id,pw); // 아이디랑 패스워드가 일치하는지 
-			
-			if(result > 0 ) { // 아이디랑 패스워드가 일치한다면
-				int deleteMember = dao.deleteMember(String id); // 아이디를 이용해서 삭제
-				
-				if(deleteMember > 0) {//삭제가 됬으면
-					System.out.println("삭제됨");
-				}else {
-					System.out.println("삭제안됨");
-				}
-				request.setAttribute("deleteMember", deleteMember);
-				request.getRequestDispatcher("leaveView.jsp").forward(request, response);
-			}
-			
-		}else if(command.equals("goToIndex.member")) {//인덱스 페이지로 가기 
-			request.getRequestDispatcher("logIndex.jsp").forward(request, response);
-		}
+		
 		
 	}
 
--- src/kh/ncs/dao/MemberDAO.java
+++ src/kh/ncs/dao/MemberDAO.java
@@ -1,30 +1,8 @@
 package kh.ncs.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
 
 public class MemberDAO {
-	private DataSource ds;
 
-	public int deleteMember(String id) { // 아이디값을 이용해서 회원탈퇴
-		String sql = "delete from sajo where id = ? ";
-		try(
-				Connection con = this.getConnection();
-				PreparedStatement pstat = con.prepareStatement(sql);
-				){
-			pstat.setString(1, id);
-			con.commit();
-			int result = pstat.executeUpdate();
-			return result;
-		}catch(Exception e) {
-			e.printStackTrace();
-		}return -1;
-	}
-
-
-
+	
 }
