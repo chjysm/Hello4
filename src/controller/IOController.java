6cda5ba34563cea6b1612f8a63d909177074b667
chjysm <chjysm2@naver.com>
2019-05-22 12:45:12 +0900

    controller update

--- src/controller/IOController.java
+++ src/controller/IOController.java
@@ -1,11 +1,16 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import kh.ncs.dao.InOutDAO;
+import kh.ncs.dto.InOutDTO;
 
 
 @WebServlet(".io")
@@ -19,12 +24,12 @@ public class IOController extends HttpServlet {
 		if(command.equals("/input.io")) {
 			String name=request.getParameter("name");
 			String msg = request.getParameter("msg");
-			InOutDTO dto = new InOutDTO(name,msg);
+			InOutDTO dto = new InOutDTO(0,name,msg);
 			int result=dao.insert(dto);
 			request.setAttribute("result", result);
 			request.getRequestDispatcher("index.jsp").forward(request, response);
 		}else if(command.equals("/output.io")) {
-			List <InOutDTO> result = dao.selectAll();
+			List <InOutDTO> result = dao.output();
 			request.setAttribute("List", result);
 			request.getRequestDispatcher("output.jsp").forward(request, response);
 		}
